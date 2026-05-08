8a5c1d312377dc9c064b21e87dd1d8e930a7660c
Karencsp <karencsp@gmail.com>
2026-05-08 11:44:43 -0300

    Actualziacion0508

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ async function loadProducts() {
 }
 
 function renderProducts(items) {
+  items.sort((a, b) => {
+    if (a.stock === 0 && b.stock > 0) return 1;
+    if (a.stock > 0 && b.stock === 0) return -1;
+    return 0;
+  });
   const container = document.getElementById("product-list");
   container.innerHTML = "";
 
@@ -18,7 +23,13 @@ function renderProducts(items) {
           <h3>${product.name}</h3>
           <p>${product.description}</p>
           <div class="price">$${product.price.toLocaleString()}</div>
-          <div class="stock">Stock: ${product.stock}</div>
+          <div class="stock">Stock: 
+          ${
+            product.stock > 0
+            ? `Stock: ${product.stock}`
+            : `<span class="sin-stock">SIN STOCK</span>`
+        }
+          </div>
           <div class="color">Color: ${product.Color}</div>
         </div>
       </div>
