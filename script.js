1df37ea5488ed7364a88f36c83efed59cd0fbcbf
Karencsp <karencsp@gmail.com>
2026-05-21 21:24:03 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -12,23 +12,22 @@ function renderProducts(items) {
     if (a.stock > 0 && b.stock === 0) return -1;
     return 0;
   });
+  const availableProducts = items.filter(product => product.stock > 0);
+
   const container = document.getElementById("product-list");
   container.innerHTML = "";
 
-  items.forEach(product => {
+  availableProducts.forEach(product => {
     container.innerHTML += `
       <div class="card">
         <img src="${product.image}" alt="${product.name}">
         <div class="card-content">
           <h3>${product.name}</h3>
           <p>${product.description}</p>
-          <div class="price">$${product.price.toLocaleString()}</div>
-          <div class="stock">Stock: 
-          ${
-            product.stock > 0
-            ? `Stock: ${product.stock}`
-            : `<span class="sin-stock">SIN STOCK</span>`
-        }
+          <div class="price">$${product.price.toLocaleString()}
+          </div>
+          <div class="stock">
+            Stock: ${product.stock}
           </div>
           <div class="color">Color: ${product.Color}</div>
         </div>
